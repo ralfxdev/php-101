7c8182ab6d04f0b871ed26a21d8a9d285dd7b371
ralfxdev <lopezalexander1019@gmail.com>
2024-03-24 09:56:14 -0600

    Create a nested or multi-dimensional array in PHP

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 $title = 'My Blog';
-$post = [
-    'title' => 'How to learn PHP',
-    'content' => 'This is how you learn PHP.',
+$posts = [
+    [
+        'title' => 'How to learn PHP',
+        'content' => 'This is how you learn PHP.',
+    ],
+    [
+        'title' => 'How to learn MySQL',
+        'content' => 'This is how you learn MySQL.',
+    ],
 ];
 $numPosts = 10;
 $numPostsDisplay = "\"$numPosts\" posts";
 ?>
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
-<h3><?= $post['title'] ?></h3>
-<p><?= $post['content'] ?></p>
+<pre><?php var_dump($posts) ?></pre>
