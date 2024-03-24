201394b6c82e8e43839a437d9fb7cca4c174f328
ralfxdev <lopezalexander1019@gmail.com>
2024-03-24 06:42:56 -0600

    loops

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo("Hello World!"); ?>
+<?php
+$title = 'My Blog';
+$numPosts = 10;
+$hasPosts = $numPosts > 0;
+$numPostsDisplay = "\"$numPosts\" posts";
+$tags = ['php', 'docker', 'mysql'];
+?>
+<h1><?= $title ?></h1>
+<h2><?= $numPostsDisplay ?></h2>
+<?php
+foreach ($tags as $tag) {
+    echo "$tag<br>";
+}
+?>
