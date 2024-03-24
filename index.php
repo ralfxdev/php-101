abac76b94c83dc6cf20de73783e5a28980cd256d
ralfxdev <lopezalexander1019@gmail.com>
2024-03-24 10:31:45 -0600

    Calling functions with return values in PHP

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <?php
+function getPosts()
+{
+    return [
+        [
+            'title' => 'How to learn PHP',
+            'content' => 'This is how you learn PHP.',
+        ],
+        [
+            'title' => 'How to learn MySQL',
+            'content' => 'This is how you learn MySQL.',
+        ],
+        [
+            'title' => 'How to learn Nginx',
+            'content' => 'This is how you learn Nginx.',
+        ],
+    ];
+}
 $title = 'My Blog';
-$posts = [
-    [
-        'title' => 'How to learn PHP',
-        'content' => 'This is how you learn PHP.',
-    ],
-    [
-        'title' => 'How to learn MySQL',
-        'content' => 'This is how you learn MySQL.',
-    ],
-];
+$posts = getPosts();
 $numPosts = count($posts);
-$postDisplay = $numPosts === 1 ? 'post' : 'posts';
-$numPostsDisplay = "$numPosts $postDisplay";
+$postText = $numPosts === 1 ? 'post' : 'posts';
+$numPostsDisplay = "$numPosts $postText";
 ?>
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
-<?php for ($i = 0; $i < $numPosts; $i++): ?>
+<?php for ($i = 0; $i < $numPosts; $i++) : ?>
     <h3><?= $posts[$i]['title'] ?></h3>
     <p><?= $posts[$i]['content'] ?></p>
 <?php endfor ?>
